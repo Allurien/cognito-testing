f6d6a4a6b78c518619e4a1c449159b14502c3159
Steffany <kielen@gmail.com>
2018-10-23 23:05:27 -0700

    building out login functions

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import Routes from "./Routes";
+import ButtonAppBar from "./navbar/nav";
+// import Login from './login';
+// import {Route, Switch} from 'react-router-dom';
 
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
-);
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isAuthenticated: false
+        };
+    }
+    userHasAuthenticated = authenticated => {
+        this.setState({ isAuthenticated: authenticated });
+    }
+
+    render (){
+        const childProps = {
+            isAuthenticated: this.state.isAuthenticated,
+            userHasAuthenticated: this.userHasAuthenticated
+        };
+        return(
+            <div>
+                <ButtonAppBar />
+                <div className="app">
+                    <h1>Welcome to React</h1>
+                </div>
+                <Routes childProps={childProps} />
+                {/* <Switch>
+                    <Route childProps={childProps} path="/login" exact component={Login} />
+                </Switch> */}
+            </div>
+        )
+    }
+}
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "YOUR_API_GATEWAY_REGION",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_bE2hNqmyh",
+      APP_CLIENT_ID: "1kl2nj3v5d8bluppb4ipgf37l1",
+      IDENTITY_POOL_ID: "us-east-2:cf60b0de-94d4-4ad8-9933-84266ceb57e0"
+    }
+  };
